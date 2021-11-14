9fdb43ed9e8d72ff68a30a61ca71baf57227811e
Sven Anders Robbestad <robbestad@gmail.com>
2021-11-14 12:30:37 +0100

    taken

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -32,15 +32,7 @@
 <span>
     <p>Token Ids can be any number between 1 and 7778.</p>
     <p>
-        Press the button and find a list of the lowest available token IDs.
-        Numbers can be taken between each update (the list is updated every 5
-        minutes).
-    </p>
-    <p class="small">
-        Why lowest? Low IDs are usually more sought after than high IDs. In
-        the end, it doesn't matter that much, but it's smart to grab the
-        lowest ones while they're available.
-    </p>
+     That's it, folks. All are taken. 
     <div>
         <h2>Stats</h2>
         <p>
@@ -54,9 +46,13 @@
     <div>
         <h2>100 first available tokens</h2>
         <p>
-            {availableIds.slice(0, 100).join(", ")}
+             That's it, folks. All are taken. 
         </p>
     </div>
+    <div>
+    <a href="https://opensea.io/collection/devs-for-revolution" target="_blank" referrer="no-referrer no-opener">Open OpenSea to buy second-hand (in a new window)</a>
+    </div>
+
     <div>
     <a href="https://etherscan.io/token/0x25ed58c027921e14d86380ea2646e3a1b5c55a8b#writeContract" target="_blank" referrer="no-referrer no-opener">Open Etherscan (in a new window)</a>
     </div>
